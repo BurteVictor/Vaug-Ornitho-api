a70f88a0a5577fff88514ff701a32802c0b683eb
BURTE Victor <victor.burte@gmail.com>
2019-12-02 15:03:16 +0100

    init vaugrenier project

---
+++ src/main/java/com/adaming/vaugrenier/VaugrenierApplication.java
@@ -0,0 +1,13 @@
+package com.adaming.vaugrenier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaugrenierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaugrenierApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/adaming/vaugrenier/VaugrenierApplications.java
@@ -0,0 +1,5 @@
+package com.adaming.vaugrenier;
+
+@SpringBootApplication
+public class VaugrenierApplications {
+}
